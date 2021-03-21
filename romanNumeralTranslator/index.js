950c9efc9b70951c09f0261747ede35c0adefb13
Miguel Angel <m.angel011192@gmail.com>
2021-03-21 11:22:41 +0100

    Roman numeral translator

--- romanNumeralTranslator/index.js
+++ romanNumeralTranslator/index.js
@@ -15,8 +15,43 @@
  * You should return `null` on invalid input.
  */
 
+const romanNumbers = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
 const translateRomanNumeral = (romanNumber) => {
-  return 60;
+  const romanCharacters = romanNumber.split("");
+  let naturalNumber = 0;
+  for (let index = 0; index < romanCharacters.length; index++) {
+    const currentLetter = romanCharacters[index];
+    const nextLetter = romanCharacters[index + 1];
+
+    const currentValue = romanNumbers[currentLetter];
+    if (!currentValue) {
+      return "Invalid Input";
+    }
+    const nextValue = romanNumbers[nextLetter];
+    if (currentValue < nextValue) {
+      naturalNumber -= currentValue;
+    } else {
+      naturalNumber += currentValue;
+    }
+  }
+  return naturalNumber;
 };
 
 console.log("IV", translateRomanNumeral("IV"));
+console.log("IX", translateRomanNumeral("IX"));
+console.log("VI", translateRomanNumeral("VI"));
+console.log("LX", translateRomanNumeral("LX"));
+console.log("AA", translateRomanNumeral("AA"));
+console.log("VII", translateRomanNumeral("VII"));
+console.log("VIII", translateRomanNumeral("VIII"));
+console.log("CD", translateRomanNumeral("CD"));
+console.log("MCMXCIX", translateRomanNumeral("MCMXCIX"));
+console.log("MMXXI", translateRomanNumeral("MMXXI"));
