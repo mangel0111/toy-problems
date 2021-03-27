7b3246435d007a795f45767cf908d52b84cdf454
Miguel Angel <m.angel011192@gmail.com>
2021-03-27 12:49:13 +0100

    Add main and class solution

--- rotateString/index.cpp
+++ rotateString/index.cpp
@@ -1,7 +1,30 @@
-class Solution
+#include <vector>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+class StringUtil
 {
 public:
-  void reverseString(vector<char> &s)
+  void reverseString(vector<char *> &s)
   {
+    int wordSize = s.size();
+    int limit = wordSize / 2;
+    for (int index = 0; index < limit; index++)
+    {
+      char *temp = s[index];
+      s[index] = s[wordSize - 1 - index];
+      s[wordSize - 1 - index] = temp;
+    }
   }
 };
+
+int main()
+{
+  vector<char *> vector = {"h", "e", "l", "l", "o"};
+  StringUtil stringUtil;
+  stringUtil.reverseString(vector);
+}
