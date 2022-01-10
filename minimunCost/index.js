97c4ad01c157e3b0c0c4d366753b73f03228c947
Miguel Angel <miguelangel@MacBook-Pro.local>
2022-01-10 23:28:58 +0100

    Improve the code using dp

--- minimunCost/index.js
+++ minimunCost/index.js
@@ -6,16 +6,48 @@ const minimunCost = (list) => {
   for (let index = 1; index < list.length - 1; index++) {
     const current = list[index];
 
-    for(let inverseIndex = list.length - 2;  inverseIndex > index + 1; inverseIndex--) {
-        const currentlast = list[inverseIndex];
-        const currentSum = currentlast + current;
-        if(currentSum < minSum) {
-            minSum = currentSum;
-        }
+    for (
+      let inverseIndex = list.length - 2;
+      inverseIndex > index + 1;
+      inverseIndex--
+    ) {
+      const currentlast = list[inverseIndex];
+      const currentSum = currentlast + current;
+      if (currentSum < minSum) {
+        minSum = currentSum;
+      }
     }
   }
   return minSum;
 };
 
+const optimizedMinimumCost = (list) => {
+  const remainingList = list.slice(1, list.length - 1);
+  const firstMinValue = Math.min(...remainingList);
+  const indexOfMinValue = remainingList.indexOf(firstMinValue);
+
+  const listToTheRigth = remainingList.slice(
+    indexOfMinValue + 1,
+    remainingList.length
+  );
+  const lastMinValueToTheRight = Math.min(...listToTheRigth);
+
+  const listToTheLeft = remainingList.slice(0, indexOfMinValue);
+  const lastMinValueToTheLeft = Math.min(...listToTheLeft);
+
+  const lastMinValue =
+    lastMinValueToTheRight < lastMinValueToTheLeft
+      ? lastMinValueToTheRight
+      : lastMinValueToTheLeft;
+
+  return lastMinValue + firstMinValue;
+};
+
 console.log(minimunCost([5, 3, 2, 4, 6, 1, 8, 3]), 3);
-console.log(minimunCost([5,2,2,1,1,2,4,5,6,2,1,9]), 3);
+console.log(optimizedMinimumCost([5, 3, 2, 4, 6, 1, 8, 3]), 3);
+
+// console.log(minimunCost([5, 2, 2, 1, 1, 2, 4, 5, 6, 2, 1, 9]), 3);
+// console.log(optimizedMinimumCost([5, 2, 2, 1, 1, 2, 4, 5, 6, 2, 1, 9]), 3);
+
+// console.log(minimunCost([5, 2, 1, 4, 3, 6]), 4);
+// console.log(optimizedMinimumCost([5, 2, 1, 4, 3, 6]), 4);
